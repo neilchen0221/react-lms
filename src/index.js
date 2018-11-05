6db3905f38e71bc9e7daf73084252da93fe96ee8
Neil Chen <sxch5319@gmail.com>
2018-11-05 21:20:10 +1100

    change to use local backend

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import axios from "axios";
 import App from "./app/App";
 
-// axios.defaults.baseURL = "http://localhost:65222";
-axios.defaults.baseURL = "https://lms1210.azurewebsites.net";
-axios.defaults.headers.common.Authorization =
-  "Bearer mY1LnE1NBk0lahJnBoNeT_GHmBm_36rWgC0yZhHfg1zW-osgz9AETVQLs66UyH7X3XEQs6_JUQcU2clAysHpDEzJk0W_bUPyK3WomCUj-thq3sgxMjnbBxjtjNhB3eWhAF5aZo8b-NaPDdgDh-T4mcSzNrh5pyVuw5b7YilNlYAQhkSR5WettvHOKbmJly_E8c0KAb8TxUmz4BbPd7yfSzrV3VWNlNrS76hDLT1_q6c";
+axios.defaults.baseURL = "http://localhost:65222";
+// axios.defaults.baseURL = "https://lms1210.azurewebsites.net";
+// axios.defaults.headers.common.Authorization =
+//   "Bearer iBnIk2QjYyTn4fVcgrGfJbBBns7wWF8b_SAqFDcxxup93ybxlChhESuZt53kPOLGX2GF-mfQfWC6M44PBnaGcbK6atcjfMdbDowQEtau0bbLqYKY42oQtfHln3sD66nh8gMrKZAuTlHMqFZsJ0Q_XOUh1dBgILxIok4_JkTkgC4tT1U_p3P1w1lWQYI_L5jKOhekiW03VmvrQfebXRFV6hIRSY8EDlJQxNNg_ZxhwQ0";
 
 ReactDOM.render(<App />, document.getElementById("index"));
