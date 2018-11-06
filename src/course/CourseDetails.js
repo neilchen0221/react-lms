14b05882d8ac89d36dcdbe3069090850943362f4
Neil Chen <sxch5319@gmail.com>
2018-11-06 23:00:26 +1100

    course detail component

--- src/course/CourseDetails.js
+++ src/course/CourseDetails.js
@@ -1,6 +1,41 @@
 import React from "react";
 import * as CourseApi from "./CourseApi";
 import Loader from "../common/Loader";
+import * as yup from "yup";
+import { pick } from "lodash/object";
+import { getValidationErrors } from "../common/Helper";
+import Notification from "../common/Notification";
+
+const schema = yup.object().shape({
+  title: yup
+    .string()
+    .max(50)
+    .label("Title")
+    .required(),
+  language: yup
+    .string()
+    .max(50)
+    .label("Language")
+    .required(),
+  fee: yup
+    .number()
+    .positive()
+    .min(10)
+    .max(5000)
+    .label("Fee")
+    .required(),
+  maxStudent: yup
+    .number()
+    .positive()
+    .min(10)
+    .max(40)
+    .label("Max students")
+    .required(),
+  description: yup
+    .string()
+    .max(250)
+    .label("Description")
+});
 
 class CourseDetails extends React.Component {
   constructor() {
@@ -11,7 +46,9 @@ class CourseDetails extends React.Component {
         title: "",
         fee: "",
         description: ""
-      }
+      },
+      validationErrors: {},
+      error: ""
     };
   }
 
@@ -40,14 +77,39 @@ class CourseDetails extends React.Component {
 
   handleSubmit = async e => {
     e.preventDefault();
+
+    const userInput = pick(this.state.course, ["title", "language", "fee", "maxStudent", "description"]);
+    try {
+      await schema.validate(userInput, {
+        abortEarly: false
+      });
+    } catch (err) {
+      const validationErrors = getValidationErrors(err);
+      this.setState({ validationErrors });
+      return;
+    }
+
     const { course } = this.state;
     if (this.isCreating()) {
       await CourseApi.createCourse(course);
+      window.location.href = "http://localhost:8080/#/courses";
     } else {
       await CourseApi.updateCourse(course.id, course);
+      this.setState({ validationErrors: {} });
+      alert("Course updated!");
     }
-    await CourseApi.createCourse(course);
-    alert("saved!");
+  };
+
+  handleConfirmDelete = async () => {
+    try {
+      await CourseApi.deleteCourse(this.getCourseId());
+      alert("Course deleted!");
+      window.location.href = "http://localhost:8080/#/courses";
+    } catch (err) {
+      this.setState({
+        error: "Error occurred while deleting the course"
+      });
+    }
   };
 
   isCreating() {
@@ -59,24 +121,70 @@ class CourseDetails extends React.Component {
 
   renderForm() {
     const {
-      course: { title, language, fee, maxStudent, description }
+      course: { title, language, fee, maxStudent, description },
+      validationErrors
     } = this.state;
     return (
       <div>
         <h1>{this.isCreating() ? "New Course" : "Course Detail"}</h1>
+        {!this.isCreating() && (
+          <button type="button" className="btn btn-danger" data-toggle="modal" data-target="#deleteCourseModal">
+            Delete Course
+          </button>
+        )}
+
+        <div className="modal fade" id="deleteCourseModal" tabIndex="-1" role="dialog" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Are you sure to continue</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">Are you sure you want to delete this course?</div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">
+                  Close
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-success"
+                  data-dismiss="modal"
+                  onClick={this.handleConfirmDelete}
+                >
+                  Yes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {this.state.error && <Notification>{this.state.error}</Notification>}
 
         <form onSubmit={this.handleSubmit}>
           <div>
             <label>Title</label>
             <input name="title" value={title} onChange={this.handleFieldChange} />
+            {validationErrors.title && <p style={{ color: "red" }}>{validationErrors.title}</p>}
           </div>
           <div>
             <label>Language</label>
-            <input name="language" value={language} onChange={this.handleFieldChange} />
+            <select name="language" value={language} onChange={this.handleFieldChange}>
+              <option>--Select--</option>
+              <option value="csharp">C#</option>
+              <option value="java">Java</option>
+              <option value="javascript">JavaScript</option>
+              <option value="go">Go</option>
+              <option value="ruby">Ruby</option>
+              <option value="python">Python</option>
+            </select>
+            {validationErrors.language && <p style={{ color: "red" }}>{validationErrors.language}</p>}
           </div>
           <div>
             <label>Fee</label>
             <input name="fee" value={fee} onChange={this.handleFieldChange} />
+            {validationErrors.fee && <p style={{ color: "red" }}>{validationErrors.fee}</p>}
           </div>
           <div>
             <label>Max Students</label>
@@ -88,6 +196,7 @@ class CourseDetails extends React.Component {
               <option value={40}>40</option>
               <option value={50}>50</option>
             </select>
+            {validationErrors.maxStudent && <p style={{ color: "red" }}>{validationErrors.maxStudent}</p>}
           </div>
           <div>
             <label>Description</label>
@@ -101,7 +210,7 @@ class CourseDetails extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container-fluid">
         {this.state.isLoading && <Loader />}
         {!this.state.isLoading && this.state.course && this.renderForm()}
         {!this.state.isLoading && !this.state.course && <h3>Course not found.</h3>}
