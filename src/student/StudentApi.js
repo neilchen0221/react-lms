1182a83f7918d0496dd29214d58f327c3fc70527
Neil Chen <sxch5319@gmail.com>
2018-11-06 23:01:07 +1100

    student api added

--- src/student/StudentApi.js
+++ src/student/StudentApi.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { pick } from "lodash/object";
 
 export function getStudents(pageNumber = 1) {
   return new Promise((resolve, reject) => {
@@ -13,4 +14,25 @@ export function getStudents(pageNumber = 1) {
       })
       .catch(reject);
   });
+}
+
+export async function getStudentById(id) {
+  const response = await axios.get(`/api/students/${id}`);
+  return response.data;
+}
+
+export async function createStudent(student) {
+  const response = await axios.post(`/api/students`, student);
+  return response.data;
+}
+
+export async function updateStudent(id, student) {
+  const data = pick(student, ["firstName", "lastName", "gender", "dateOfBirth", "email", "credit"]);
+  const response = await axios.put(`/api/students/${id}`, data);
+  return response.data;
+}
+
+export async function deleteStudent(id) {
+  const response = await axios.delete(`/api/students/${id}`);
+  return response.data;
 }
