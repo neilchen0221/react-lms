8868ceb1ace9a78c7ed070d6a569208c0bcb70ab
Neil Chen <sxch5319@gmail.com>
2018-11-07 20:47:52 +1100

    style updated

--- src/course/CourseList.js
+++ src/course/CourseList.js
@@ -2,29 +2,39 @@ import React from "react";
 import Loader from "../common/Loader";
 import * as CourseApi from "./CourseApi";
 import { Link } from "react-router-dom";
+import Notification from "../common/Notification";
 
 class CourseList extends React.Component {
   constructor() {
     super();
     this.state = {
-      isLoading: true,
+      isLoading: false,
+      error: "",
       courses: []
     };
   }
 
   async componentDidMount() {
-    const courses = await CourseApi.getCourses();
-    this.setState({ isLoading: false, courses: courses || [] });
+    this.setState({ isLoading: true });
+    try {
+      const courses = await CourseApi.getCourses();
+      this.setState({ isLoading: false, courses: courses || [] });
+    } catch (e) {
+      this.setState({ isLoading: false, error: "Something went wrong while loading courses..." });
+    }
   }
 
   renderCourses() {
     return (
       <div className="row">
+        {!this.state.courses.length && <h3>No Course...</h3>}
         {this.state.courses.map(c => {
           return (
             <div key={c.id} className="col-sm-4" style={{ padding: "1rem" }}>
               <div className="card">
-                <div className="card-header">{c.title}</div>
+                <div className="card-header">
+                  <h5>{c.title}</h5>
+                </div>
                 <div className="card-body">
                   <p className="card-text">{c.description}</p>
                   <Link className="btn btn-primary" to={`/courses/${c.id}`}>
@@ -41,14 +51,14 @@ class CourseList extends React.Component {
 
   render() {
     return (
-      <div className="container-fluid">
+      <div className="lms-list__container">
         <h1>Course List</h1>
-        <Link className="btn btn-primary" to={`/courses/create`}>
+        <Link className="btn btn-primary my-3" to={`/courses/create`}>
           New Couse
         </Link>
+        {this.state.error && <Notification>{this.state.error}</Notification>}
         {this.state.isLoading && <Loader />}
-        {!this.state.isLoading && this.state.courses.length && this.renderCourses()}
-        {!this.state.isLoading && !this.state.courses.length && <h3>No Course...</h3>}
+        {!this.state.isLoading && !this.state.error && this.renderCourses()}
       </div>
     );
   }
--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,5 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="lms-list__container mt-5 p-5">
+      <h1 className="text-center mb-5">Welcome to LMS</h1>
+
+      <div className="row justify-content-around">
+        <div className="col-sm-5">
+          <div className="card text-white bg-info mb-5">
+            <div className="card-header">
+              <h3>Course</h3>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title mb-5">All kinds of courses needed for IT industry</h5>
+              <Link className="btn btn-outline-light mr-3" to={`/courses`}>
+                Course List
+              </Link>
+              <Link className="btn btn-outline-light" to={`/courses/create`}>
+                New Course
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-5">
+          <div className="card text-white bg-success mb-5">
+            <div className="card-header">
+              <h3>Lecturers</h3>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title mb-5">Best lecturers in IT world</h5>
+              <Link className="btn btn-outline-light mr-3" to={`/lecturers`}>
+                Lecturer List
+              </Link>
+              <Link className="btn btn-outline-light" to={`/lecturers/create`}>
+                New Lecturer
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row justify-content-around">
+        <div className="col-sm-5">
+          <div className="card text-white bg-warning mb-5">
+            <div className="card-header">
+              <h3>Student</h3>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title mb-5">Hard-working and smart students</h5>
+              <Link className="btn btn-outline-light mr-3" to={`/students`}>
+                Student List
+              </Link>
+              <Link className="btn btn-outline-light" to={`/students/create`}>
+                New Student
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
