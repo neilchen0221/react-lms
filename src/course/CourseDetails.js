214da1821134612a870f461af3e226e629a171d7
Neil Chen <sxch5319@gmail.com>
2018-11-07 20:47:02 +1100

    style updated

--- src/course/CourseDetails.js
+++ src/course/CourseDetails.js
@@ -5,6 +5,7 @@ import * as yup from "yup";
 import { pick } from "lodash/object";
 import { getValidationErrors } from "../common/Helper";
 import Notification from "../common/Notification";
+import { Link } from "react-router-dom";
 
 const schema = yup.object().shape({
   title: yup
@@ -91,12 +92,20 @@ class CourseDetails extends React.Component {
 
     const { course } = this.state;
     if (this.isCreating()) {
-      await CourseApi.createCourse(course);
-      window.location.href = "http://localhost:8080/#/courses";
+      try {
+        await CourseApi.createCourse(course);
+        window.location.href = "http://localhost:8080/#/courses";
+      } catch (e) {
+        this.setState({ validationErrors: {}, error: "Someting went wrong while creating course..." });
+      }
     } else {
-      await CourseApi.updateCourse(course.id, course);
-      this.setState({ validationErrors: {} });
-      alert("Course updated!");
+      try {
+        await CourseApi.updateCourse(course.id, course);
+        this.setState({ validationErrors: {} });
+        alert("Course updated!");
+      } catch (e) {
+        this.setState({ validationErrors: {}, error: "Someting went wrong while saving course..." });
+      }
     }
   };
 
@@ -126,7 +135,6 @@ class CourseDetails extends React.Component {
     } = this.state;
     return (
       <div>
-        <h1>{this.isCreating() ? "New Course" : "Course Detail"}</h1>
         {!this.isCreating() && (
           <button type="button" className="btn btn-danger" data-toggle="modal" data-target="#deleteCourseModal">
             Delete Course
@@ -160,49 +168,86 @@ class CourseDetails extends React.Component {
           </div>
         </div>
 
-        {this.state.error && <Notification>{this.state.error}</Notification>}
-
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label>Title</label>
-            <input name="title" value={title} onChange={this.handleFieldChange} />
-            {validationErrors.title && <p style={{ color: "red" }}>{validationErrors.title}</p>}
+        <form className="lms-form__container" onSubmit={this.handleSubmit}>
+          <div className="form-group row">
+            <label className="col-sm-3 font-weight-bold">Title</label>
+            <span className="col-sm-9">
+              <input
+                className="form-control"
+                name="title"
+                value={title}
+                onChange={this.handleFieldChange}
+                placeholder="Title"
+              />
+              {validationErrors.title && <div className="text-danger">{validationErrors.title}</div>}
+            </span>
           </div>
-          <div>
-            <label>Language</label>
-            <select name="language" value={language} onChange={this.handleFieldChange}>
-              <option>--Select--</option>
-              <option value="csharp">C#</option>
-              <option value="java">Java</option>
-              <option value="javascript">JavaScript</option>
-              <option value="go">Go</option>
-              <option value="ruby">Ruby</option>
-              <option value="python">Python</option>
-            </select>
-            {validationErrors.language && <p style={{ color: "red" }}>{validationErrors.language}</p>}
+          <div className="form-group row">
+            <label className="col-sm-3 font-weight-bold">Language</label>
+            <span className="col-sm-9">
+              <select className="form-control w-25" name="language" value={language} onChange={this.handleFieldChange}>
+                <option>--Select--</option>
+                <option value="csharp">C#</option>
+                <option value="java">Java</option>
+                <option value="javascript">JavaScript</option>
+                <option value="go">Go</option>
+                <option value="ruby">Ruby</option>
+                <option value="python">Python</option>
+              </select>
+              {validationErrors.language && <div className="text-danger">{validationErrors.language}</div>}
+            </span>
           </div>
-          <div>
-            <label>Fee</label>
-            <input name="fee" value={fee} onChange={this.handleFieldChange} />
-            {validationErrors.fee && <p style={{ color: "red" }}>{validationErrors.fee}</p>}
+          <div className="form-group row">
+            <label className="col-sm-3 font-weight-bold">Fee($)</label>
+            <span className="col-sm-9">
+              <input
+                className="form-control"
+                name="fee"
+                value={fee}
+                onChange={this.handleFieldChange}
+                placeholder="Fee($)"
+              />
+              {validationErrors.fee && <div className="text-danger">{validationErrors.fee}</div>}
+            </span>
           </div>
-          <div>
-            <label>Max Students</label>
-            <select name="maxStudent" value={maxStudent} onChange={this.handleFieldChange}>
-              <option>--Select--</option>
-              <option value={10}>10</option>
-              <option value={20}>20</option>
-              <option value={30}>30</option>
-              <option value={40}>40</option>
-              <option value={50}>50</option>
-            </select>
-            {validationErrors.maxStudent && <p style={{ color: "red" }}>{validationErrors.maxStudent}</p>}
+          <div className="form-group row">
+            <label className="col-sm-3 font-weight-bold">Max Students</label>
+            <span className="col-sm-9">
+              <select
+                className="form-control w-25"
+                name="maxStudent"
+                value={maxStudent}
+                onChange={this.handleFieldChange}
+              >
+                <option>--Select--</option>
+                <option value={10}>10</option>
+                <option value={20}>20</option>
+                <option value={30}>30</option>
+                <option value={40}>40</option>
+                <option value={50}>50</option>
+              </select>
+              {validationErrors.maxStudent && <div className="text-danger">{validationErrors.maxStudent}</div>}
+            </span>
           </div>
-          <div>
-            <label>Description</label>
-            <textarea name="description" value={description} onChange={this.handleFieldChange} />
+          <div className="form-group row">
+            <label className="col-sm-3 font-weight-bold">Description</label>
+            <span className="col-sm-9">
+              <textarea
+                className="form-control"
+                name="description"
+                value={description}
+                onChange={this.handleFieldChange}
+                placeholder="Description"
+              />
+              {validationErrors.description && <div className="text-danger">{validationErrors.description}</div>}
+            </span>
           </div>
-          <button type="submit">{this.isCreating() ? "Create" : "Save"}</button>
+          <button className="btn btn-primary" type="submit">
+            {this.isCreating() ? "Create" : "Save"}
+          </button>
+          <Link className="btn btn-light ml-3" to="/courses">
+            Close
+          </Link>
         </form>
       </div>
     );
@@ -210,7 +255,9 @@ class CourseDetails extends React.Component {
 
   render() {
     return (
-      <div className="container-fluid">
+      <div className="lms-container">
+        <h1>{this.isCreating() ? "New Course" : "Course Detail"}</h1>
+        {this.state.error && <Notification>{this.state.error}</Notification>}
         {this.state.isLoading && <Loader />}
         {!this.state.isLoading && this.state.course && this.renderForm()}
         {!this.state.isLoading && !this.state.course && <h3>Course not found.</h3>}
