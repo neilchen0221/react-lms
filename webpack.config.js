f29a5056e877fe9561e39c6886e594ce240cc73f
Neil Chen <sxch5319@gmail.com>
2018-11-13 20:45:31 +1100

    url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,14 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        use: [
+          {
+            loader: "url-loader"
+          }
+        ]
       }
     ]
   },
