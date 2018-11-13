a9feeee0648f3c7d5c702e2476c04e7b3a2a79a1
Neil Chen <sxch5319@gmail.com>
2018-11-13 20:46:58 +1100

    loader added

--- src/common/Loader.js
+++ src/common/Loader.js
@@ -1,5 +1,6 @@
 import React from "react";
+import "./Loader.css";
 
 export default function Loader() {
-  return <h3 className="text-center">Loading...</h3>;
+  return <div className="loader" />;
 }
