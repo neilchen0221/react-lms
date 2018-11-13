94a761bd33eb6e24102e79ee41d5055769430276
Neil Chen <sxch5319@gmail.com>
2018-11-13 20:47:30 +1100

    deleted

--- src/common/PageLoader.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function() {
-  return <h3>Loading...</h3>;
-}
--- src/common/Spinner.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Spinner() {
-  return <h1>Spinner...</h1>;
-}
