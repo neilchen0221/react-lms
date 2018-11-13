fbf6340b2a56de24e4e2d5e97dc123f8c487a8b4
Neil Chen <sxch5319@gmail.com>
2018-11-13 20:47:22 +1100

    notification takes props

--- src/common/Notification.js
+++ src/common/Notification.js
@@ -1,11 +1,29 @@
 import React from "react";
+import classnames from "classnames";
 
 export default class Notification extends React.PureComponent {
   constructor() {
     super();
-    this.state = {};
+    this.state = { shown: true };
   }
+
+  handleClose = () => {
+    this.setState({ shown: false });
+  };
+
   render() {
-    return <div className="alert alert-danger my-3">{this.props.children}</div>;
+    if (!this.state.shown) {
+      return null;
+    }
+
+    const { type, children } = this.props;
+    return (
+      <div className={classnames("alert", "my-3", `alert-${type || "danger"}`)}>
+        {children}
+        <button onClick={this.handleClose} className="close float-right">
+          <span>&times;</span>
+        </button>
+      </div>
+    );
   }
 }
