4ea010550591f937f232b27a9674e7dcba5db430
Neil Chen <sxch5319@gmail.com>
2018-11-19 14:04:51 +1100

    dashboard style updated

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -5,37 +5,44 @@ export default function Dashboard() {
   return (
     <div className="lms-list__container mt-5 p-4">
       <h1 className="text-center mb-5">
-        Welcome to <img src="https://s3-ap-southeast-2.amazonaws.com/lms-webapp/lms_brand.png" height="60px" />
+        Welcome to{" "}
+        <img src="https://s3-ap-southeast-2.amazonaws.com/lms.techcrafting.net/lms_brand.png" height="60px" />
       </h1>
 
-      <div className="row justify-content-around ">
-        <div className="col-sm-5 px-0 px-sm-5">
+      <div className="row justify-content-around">
+        <div className="col-md-6 col-lg-5 px-0 px-sm-5">
           <div className="card text-white bg-info mb-5 lms-shadow">
             <div className="card-header">
-              <h3>Course</h3>
+              <h3>
+                <i className="fas fa-book mr-2" />
+                Course
+              </h3>
             </div>
             <div className="card-body">
               <h5 className="card-title mb-5">All kinds of courses needed for IT industry</h5>
               <Link className="btn btn-outline-light mr-3" to={`/courses`}>
                 Course List
               </Link>
-              <Link className="btn btn-outline-light my-2 my-sm-0" to={`/courses/create`}>
+              <Link className="btn btn-outline-light my-2 my-lg-0" to={`/courses/create`}>
                 New Course
               </Link>
             </div>
           </div>
         </div>
-        <div className="col-sm-5 px-0 px-sm-5">
+        <div className="col-md-6 col-lg-5 px-0 px-sm-5">
           <div className="card text-white bg-success mb-5 lms-shadow">
             <div className="card-header">
-              <h3>Lecturers</h3>
+              <h3>
+                <i className="fas fa-chalkboard-teacher mr-2" />
+                Lecturers
+              </h3>
             </div>
             <div className="card-body">
               <h5 className="card-title mb-5">Best lecturers in IT world</h5>
               <Link className="btn btn-outline-light mr-3" to={`/lecturers`}>
                 Lecturer List
               </Link>
-              <Link className="btn btn-outline-light my-2 my-sm-0" to={`/lecturers/create`}>
+              <Link className="btn btn-outline-light my-2 my-lg-0" to={`/lecturers/create`}>
                 New Lecturer
               </Link>
             </div>
@@ -43,17 +50,20 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="row justify-content-around">
-        <div className="col-sm-5 px-0 px-sm-5">
+        <div className="col-md-6 col-lg-5 px-0 px-sm-5">
           <div className="card text-dark bg-warning mb-5 lms-shadow">
             <div className="card-header">
-              <h3>Student</h3>
+              <h3>
+                <i className="fas fa-graduation-cap mr-2" />
+                Student
+              </h3>
             </div>
             <div className="card-body">
               <h5 className="card-title mb-5">Hard-working and smart students</h5>
               <Link className="btn btn-outline-dark mr-3" to={`/students`}>
                 Student List
               </Link>
-              <Link className="btn btn-outline-dark my-2 my-sm-0" to={`/students/create`}>
+              <Link className="btn btn-outline-dark my-2 my-lg-0" to={`/students/create`}>
                 New Student
               </Link>
             </div>
