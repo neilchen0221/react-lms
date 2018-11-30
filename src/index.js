2f9f2076a9ec0d213f9172ad4e4747505ab7c517
Neil Chen <sxch5319@gmail.com>
2018-11-30 23:30:39 +1100

    redux store provider added for App component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import axios from "axios";
 import App from "./app/App";
 import { redirect, getApiUrl } from "./common/Helper";
+import { Provider } from "react-redux";
+import store from "./store";
 
 axios.defaults.baseURL = getApiUrl();
 
@@ -20,4 +22,9 @@ axios.interceptors.response.use(
   }
 );
 
-ReactDOM.render(<App />, document.getElementById("index"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("index")
+);
